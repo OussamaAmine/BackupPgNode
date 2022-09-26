a6f4d7393d342f869b031c36ce3d53918d5cabc6
Oussama Amine Bentama <oa.bentama@gmail.com>
2022-09-26 05:13:46 +0100

    fixes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const backup = require("./src/backup/backup");
+const backup = require("./src/restore/restore");
 
 const app = express();
 
 app.use("/pg/backup", backup);
+app.use("/pg/restore", restore);
 
 const server = app.listen();
 process.on("unhandledRejection", (err) => {
