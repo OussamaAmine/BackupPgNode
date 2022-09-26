a35d3dc7769980e75b5e3274501c4085ff70fdea
Oussama Amine Bentama <oa.bentama@gmail.com>
2022-09-26 06:07:14 +0100

    f

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-const express = require("express");
-const backup = require("./src/backup/backup");
-const restore = require("./src/restore/restore");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const backup = require('./src/backup/backup');
+const restore = require('./src/restore/restore');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use("/pg/restor", restore);
-app.use("/pg/backup", backup);
+app.use('/pg/restore', restore);
+app.use('/pg/backup', backup);
 
 const server = app.listen();
-process.on("unhandledRejection", (err) => {
+
+process.on('unhandledRejection', (err) => {
   console.error(err);
-  console.log("Unhandled Rejection  ");
+  console.log('Unhandled Rejection  ');
   server.close(() => {
     process.exit(1);
   });
 });
-process.on("SIGTERM", () => {
-  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
   server.close(() => {
-    console.log("💥 Process terminated!");
+    console.log('💥 Process terminated!');
   });
 });
--- src/restore/restore.js
+++ src/restore/restore.js
@@ -1,58 +1,59 @@
-const express = require("express");
-const path = require("path");
-const { execute } = require("@getvim/execute");
-const fs = require("fs");
-const compress = require("gzipme");
-const { promisify } = require("util");
-const dotenv = require("dotenv");
-const multer = require("multer");
+const express = require('express');
+const path = require('path');
+const { execute } = require('@getvim/execute');
+const fs = require('fs');
+const compress = require('gzipme');
+const { promisify } = require('util');
+const dotenv = require('dotenv');
+const multer = require('multer');
 
 const router = express.Router();
 
-const multerMemoryStorage = multer.memoryStorage();
+// const multerMemoryStorage = multer.memoryStorage();
 
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("application/gzip")) {
-    cb(null, true);
-  } else {
-    cb(new Error("error"), false);
-  }
-};
+// const multerFilter = (req, file, cb) => {
+//   if (file.mimetype.startsWith("application/gzip")) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("error"), false);
+//   }
+// };
 
-const uploadToMemory = multer({
-  storage: multerMemoryStorage,
-  fileFilter: multerFilter,
-});
+// const uploadToMemory = multer({
+//   storage: multerMemoryStorage,
+//   fileFilter: multerFilter,
+// });
 
-router.post("/", uploadToMemory.single("file"), async (req, res) => {
-  dotenv.config();
-  const database = process.env.DB_NAME;
+router.post('/', (req, res) => {
+  //     uploadToMemory.single("file")
+  //   dotenv.config();
+  //   const database = process.env.DB_NAME;
 
-  const writeFileAsync = promisify(fs.writeFile);
+  //   const writeFileAsync = promisify(fs.writeFile);
 
-  try {
-    if (!req.file) {
-      throw new Error("you must upload file ");
-    }
-    const date = new Date();
-    const currentDate = `${date.getFullYear()}.${
-      date.getMonth() + 1
-    }.${date.getDate()}.${date.getHours()}.${date.getMinutes()}`;
+  //   try {
+  //     if (!req.file) {
+  //       throw new Error("you must upload file ");
+  //     }
+  //     const date = new Date();
+  //     const currentDate = `${date.getFullYear()}.${
+  //       date.getMonth() + 1
+  //     }.${date.getDate()}.${date.getHours()}.${date.getMinutes()}`;
 
-    const fileName = `upload-backup-${currentDate}.sql.gz`;
-    let filePath = path.join(__dirname, "../", "../", "upload", fileName);
+  //     const fileName = `upload-backup-${currentDate}.sql.gz`;
+  //     let filePath = path.join(__dirname, "../", "../", "upload", fileName);
 
-    await writeFileAsync(filePath, req.file.buffer);
+  //     await writeFileAsync(filePath, req.file.buffer);
 
-    await execute(`pg_restore -cC -d ${database} ${filePath}`);
-    console.log("Restored");
-    res.status(200).json({
-      status: "succes",
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500);
-  }
+  //     await execute(`pg_restore -cC -d ${database} ${filePath}`);
+  console.log('Restored');
+  res.status(200).json({
+    status: 'succes',
+  });
+  //   } catch (err) {
+  //     console.log(err);
+  //     res.status(500);
+  //   }
 });
 
 module.exports = router;
