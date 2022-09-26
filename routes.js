cf5e8fa59bfbc940409817ba1a391cd825baf51e
Oussama Amine Bentama <oa.bentama@gmail.com>
2022-09-26 06:45:50 +0100

    fixes

--- routes.js
+++ routes.js
@@ -5,6 +5,6 @@ const restore = require('./src/restore/restore');
 const router = express.Router();
 
 router.route('/backup').get(backup.backup);
-router.route('/restore').post(restore.restore);
+router.route('/restore').post(restore.uploadMulter, restore.restore);
 
 module.exports = router;
--- src/restore/restore.js
+++ src/restore/restore.js
@@ -6,48 +6,49 @@ const { promisify } = require('util');
 const dotenv = require('dotenv');
 const multer = require('multer');
 
-// const multerMemoryStorage = multer.memoryStorage();
+const multerMemoryStorage = multer.memoryStorage();
 
-// const multerFilter = (req, file, cb) => {
-//   if (file.mimetype.startsWith("application/gzip")) {
-//     cb(null, true);
-//   } else {
-//     cb(new Error("error"), false);
-//   }
-// };
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('application/gzip')) {
+    cb(null, true);
+  } else {
+    cb(new Error('error'), false);
+  }
+};
 
-// const uploadToMemory = multer({
-//   storage: multerMemoryStorage,
-//   fileFilter: multerFilter,
-// });
-exports.restore = (req, res) => {
-  //     uploadToMemory.single("file")
-  //   dotenv.config();
-  //   const database = process.env.DB_NAME;
+const uploadToMemory = multer({
+  storage: multerMemoryStorage,
+  fileFilter: multerFilter,
+});
 
-  //   const writeFileAsync = promisify(fs.writeFile);
+exports.uploadMulter = uploadToMemory.single('file');
+exports.restore = async (req, res) => {
+  dotenv.config();
+  const database = process.env.DB_NAME;
 
-  //   try {
-  //     if (!req.file) {
-  //       throw new Error("you must upload file ");
-  //     }
-  //     const date = new Date();
-  //     const currentDate = `${date.getFullYear()}.${
-  //       date.getMonth() + 1
-  //     }.${date.getDate()}.${date.getHours()}.${date.getMinutes()}`;
+  const writeFileAsync = promisify(fs.writeFile);
 
-  //     const fileName = `upload-backup-${currentDate}.sql.gz`;
-  //     let filePath = path.join(__dirname, "../", "../", "upload", fileName);
+  try {
+    if (!req.file) {
+      throw new Error('you must upload file ');
+    }
+    const date = new Date();
+    const currentDate = `${date.getFullYear()}.${
+      date.getMonth() + 1
+    }.${date.getDate()}.${date.getHours()}.${date.getMinutes()}`;
 
-  //     await writeFileAsync(filePath, req.file.buffer);
+    const fileName = `upload-backup-${currentDate}.sql.gz`;
+    let filePath = path.join(__dirname, '../', '../', 'upload', fileName);
 
-  //     await execute(`pg_restore -cC -d ${database} ${filePath}`);
-  console.log('Restored');
-  res.status(200).json({
-    status: 'succes',
-  });
-  //   } catch (err) {
-  //     console.log(err);
-  //     res.status(500);
-  //   }
+    await writeFileAsync(filePath, req.file.buffer);
+
+    await execute(`pg_restore -cC -d ${database} ${filePath}`);
+    console.log('Restored');
+    res.status(200).json({
+      status: 'succes',
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
 };
