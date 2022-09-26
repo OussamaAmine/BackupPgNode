21eec51fe549be446d4f037ac8f4aec64c792475
Oussama Amine Bentama <oa.bentama@gmail.com>
2022-09-26 05:56:52 +0100

    f

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const backup = require("./src/backup/backup");
 const restore = require("./src/restore/restore");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/pg/restor", restore);
 app.use("/pg/backup", backup);
