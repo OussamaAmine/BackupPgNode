45e1231f32d657c9265d4c5836e1d57370095c1f
Oussama Amine Bentama <oa.bentama@gmail.com>
2022-09-26 05:47:50 +0100

    f

--- src/restore/restore.js
+++ src/restore/restore.js
@@ -29,39 +29,31 @@ const writeFileAsync = promisify(fs.writeFile);
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get((req, res) => {
-    console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+router.route("/").post(async (req, res) => {
+  uploadToMemory.single("file");
+  try {
+    if (!req.file) {
+      throw new Error("you must upload file ");
+    }
+    const date = new Date();
+    const currentDate = `${date.getFullYear()}.${
+      date.getMonth() + 1
+    }.${date.getDate()}.${date.getHours()}.${date.getMinutes()}`;
+
+    const fileName = `upload-backup-${currentDate}.sql.gz`;
+    let filePath = path.join(__dirname, "../", "../", "upload", fileName);
+
+    await writeFileAsync(filePath, req.file.buffer);
+
+    await execute(`pg_restore -cC -d ${database} ${filePath}`);
+    console.log("Restored");
     res.status(200).json({
       status: "succes",
     });
-  })
-  .post(async (req, res) => {
-    uploadToMemory.single("file");
-    try {
-      if (!req.file) {
-        throw new Error("you must upload file ");
-      }
-      const date = new Date();
-      const currentDate = `${date.getFullYear()}.${
-        date.getMonth() + 1
-      }.${date.getDate()}.${date.getHours()}.${date.getMinutes()}`;
-
-      const fileName = `upload-backup-${currentDate}.sql.gz`;
-      let filePath = path.join(__dirname, "../", "../", "upload", fileName);
-
-      await writeFileAsync(filePath, req.file.buffer);
-
-      await execute(`pg_restore -cC -d ${database} ${filePath}`);
-      console.log("Restored");
-      res.status(200).json({
-        status: "succes",
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500);
-    }
-  });
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
+});
 
 module.exports = router;
